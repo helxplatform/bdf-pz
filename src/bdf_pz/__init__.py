58315ae5e9fbcb8e8924206394f872ba09c2a9b8
frostyfan109 <groupe@renci.org>
2025-10-10 16:08:50 -0400

    Add new package reqs, env file, and add package-level logging

--- src/bdf_pz/__init__.py
+++ src/bdf_pz/__init__.py
@@ -1,3 +1,14 @@
 # SPDX-FileCopyrightText: 2024-present Brandon Rose <rose.brandon.m@gmail.com>
 #
 # SPDX-License-Identifier: MIT
+
+import logging
+import os
+
+# Define a package-level logging config for bdf_pz
+package_logger = logging.getLogger(__name__)
+package_logger.setLevel(os.environ.get("LOG_LEVEL", "INFO").upper())
+handler = logging.StreamHandler()
+formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+handler.setFormatter(formatter)
+package_logger.addHandler(handler)
